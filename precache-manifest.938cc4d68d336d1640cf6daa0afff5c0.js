b7dfbb3142fe81d3811e7af22c36475b27f68bdb
Jacky Chen <jacky.chen.6@gmail.com>
2020-07-06 19:03:34 -0400

    Updates

---
+++ precache-manifest.938cc4d68d336d1640cf6daa0afff5c0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e07547b05fb45afe11b70c66098664",
+    "url": "/portfolio_react/index.html"
+  },
+  {
+    "revision": "331cda024afa098565c7",
+    "url": "/portfolio_react/static/css/main.d0d2ed14.chunk.css"
+  },
+  {
+    "revision": "54f3a51154154dad9b9a",
+    "url": "/portfolio_react/static/js/2.0f2f06cf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio_react/static/js/2.0f2f06cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "331cda024afa098565c7",
+    "url": "/portfolio_react/static/js/main.cc541ab1.chunk.js"
+  },
+  {
+    "revision": "31d5ba04d1fafd9a5a35",
+    "url": "/portfolio_react/static/js/runtime-main.17440bfd.js"
+  },
+  {
+    "revision": "70d5569b0f33b73f7751a92b8207cddc",
+    "url": "/portfolio_react/static/media/bank.70d5569b.jpg"
+  },
+  {
+    "revision": "d27764f530dddaa1c4f06265214f5d06",
+    "url": "/portfolio_react/static/media/clique.d27764f5.jpg"
+  },
+  {
+    "revision": "7c1bada4b2557337439d7487309318d0",
+    "url": "/portfolio_react/static/media/duck.7c1bada4.png"
+  },
+  {
+    "revision": "e82b5287625dc596881f3b89ccdbc000",
+    "url": "/portfolio_react/static/media/grocery.e82b5287.jpg"
+  },
+  {
+    "revision": "ae669243ce7ecceb233ff1e6445af1cf",
+    "url": "/portfolio_react/static/media/transplant.ae669243.jpg"
+  }
+]);
