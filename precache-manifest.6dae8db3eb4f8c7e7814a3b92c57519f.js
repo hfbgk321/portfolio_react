eea486cf15017d01f058037a2c8228a2039a2563
Jacky Chen <jacky.chen.6@gmail.com>
2020-07-06 19:30:20 -0400

    Updates

---
+++ precache-manifest.6dae8db3eb4f8c7e7814a3b92c57519f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4240a29ac017c7056efdc1b60e54fc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "331cda024afa098565c7",
+    "url": "/static/css/main.d0d2ed14.chunk.css"
+  },
+  {
+    "revision": "54f3a51154154dad9b9a",
+    "url": "/static/js/2.0f2f06cf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0f2f06cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "331cda024afa098565c7",
+    "url": "/static/js/main.cc541ab1.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "70d5569b0f33b73f7751a92b8207cddc",
+    "url": "/static/media/bank.70d5569b.jpg"
+  },
+  {
+    "revision": "d27764f530dddaa1c4f06265214f5d06",
+    "url": "/static/media/clique.d27764f5.jpg"
+  },
+  {
+    "revision": "7c1bada4b2557337439d7487309318d0",
+    "url": "/static/media/duck.7c1bada4.png"
+  },
+  {
+    "revision": "e82b5287625dc596881f3b89ccdbc000",
+    "url": "/static/media/grocery.e82b5287.jpg"
+  },
+  {
+    "revision": "ae669243ce7ecceb233ff1e6445af1cf",
+    "url": "/static/media/transplant.ae669243.jpg"
+  }
+]);
