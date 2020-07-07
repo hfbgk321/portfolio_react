a434d01211464cb807b5e8dcb772927094f54610
Jacky Chen <jacky.chen.6@gmail.com>
2020-07-06 20:04:41 -0400

    hello

--- src/index.js
+++ src/index.js
@@ -6,39 +6,8 @@ import { BrowserRouter as Router,Route} from 'react-router-dom';
 import Home from './components/home';
 import Contact from './components/contact';
 import Projects from './components/projects';
-ReactDOM.render(
-  <Router basename ={process.env.PUBLIC_URL}>
-    <App />
-    <Route exact path ="/">
-          <Home/>
-        </Route>
-      <Route exact path ="/home">
-        <Home/>
-      </Route>
 
-      <Route exact path ="/projects">
-        <Projects/>
-      </Route>
-
-      <Route exact path ="/contact">
-        <Contact/>
-      </Route>
-
-
-      <Route exact path ="/my_passions">
-        <Home/>
-      </Route>
-
-      <Route exact path ="/my_projects">
-        <Projects/>
-      </Route>
-
-      <Route exact path ="/contact_me">
-        <Contact/>
-      </Route>
-  </Router>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
